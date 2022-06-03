94c66e6d00832b0c9c9f10d82bd238e27454c87a
Xenometon <106739081+Xenometon@users.noreply.github.com>
2022-06-03 18:21:54 +0530

    Add files via upload

---
+++ compute.py
@@ -0,0 +1,18 @@
+#Simple Computational Operations through Python
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+print("Input num1=",x)
+print("Input num2=",y)
+print("Computing Calculations")
+a=x*y
+print("x.y=",a)
+b=x+y
+print("x+y=",b)
+c=x-y
+print("x-y=",c)
+d=x*x+y*y
+print("x squared + y squared:",d)
+e=x*x-y*y
+print("x squared - y squared is:",e)
+print("Calculation terminated")
+#End of program
